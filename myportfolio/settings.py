9d42eb38648faa04be67d5e4137a05b2a705152d
Sandzhi-Garia Ochirov <sandzhi.garia@gmail.com>
2025-06-29 19:26:10 -0400

    database

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -1,6 +1,5 @@
 from pathlib import Path
-import os
-
+import dj_database_url
 BASE_DIR = Path(__file__).resolve().parent.parent
 import os
 
@@ -51,15 +50,14 @@ TEMPLATES = [
 WSGI_APPLICATION = 'myportfolio.wsgi.application'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-# Database (по умолчанию SQLite)
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', 'sqlite:///' + str(BASE_DIR / 'db.sqlite3')),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
-# Password validation
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
